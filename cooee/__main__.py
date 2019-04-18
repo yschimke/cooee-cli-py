46722c5cd90429064be282440855db9fab36113a
Yuri Schimke <yuri@schimke.ee>
2019-04-18 19:53:16 +0100

    Cleanup

--- cooee/__main__.py
+++ cooee/__main__.py
@@ -5,19 +5,26 @@ from typing import List, Dict, Any
 
 import click
 import requests
+from requests.auth import AuthBase
 
 from .bearer import HTTPBearerAuth
 
 headers = {'user-agent': 'cooee/0.0.1'}
-auth = None
+auth: AuthBase = None
+local: bool = False
+
+
+def set_local(ctx, param, value):
+    global local
+    local = value
 
 
 @click.command()
 @click.option('--login', is_flag=True, help='Login to coo.ee')
-@click.option('--local', '-l', is_flag=True, help='Use local services')
-@click.option('--fish-complete', is_flag=True, help='Use local services')
+@click.option('--local', '-l', is_flag=True, help='Use local services', callback=set_local, expose_value=False)
+@click.option('--fish-complete', is_flag=True, help='Fish shell completion')
 @click.argument('arguments', nargs=-1)
-def main(login: bool = False, local: bool = False, fish_complete: bool = False, arguments: List[str] = None):
+def main(login: bool = False, fish_complete: bool = False, arguments: List[str] = None):
     """https://coo.ee command line."""
     global auth
 
@@ -27,17 +34,17 @@ def main(login: bool = False, local: bool = False, fish_complete: bool = False, 
             auth = HTTPBearerAuth(f.read().strip())
 
     if login:
-        webbrowser.open(web_path(local=local))
+        webbrowser.open(web_path())
     elif fish_complete:
-        complete(arguments, local=local, fish=fish_complete)
+        complete(arguments, fish=fish_complete)
     elif not arguments:
-        todo(local=local)
+        todo()
     else:
-        launch(arguments, local=local)
+        launch(arguments)
 
 
-def launch(arguments: List[str], local: bool = False):
-    path = api_path(f"/api/v0/goinfo?q={'+'.join(arguments)}", local=local)
+def launch(arguments: List[str]):
+    path = api_path(f"/api/v0/goinfo?q={'+'.join(arguments)}")
     r = requests.get(path, headers=headers, auth=auth)
 
     r.raise_for_status()
@@ -50,8 +57,8 @@ def launch(arguments: List[str], local: bool = False):
         print(result)
 
 
-def todo(local: bool = False):
-    path = api_path(f"/api/v0/todo", local=local)
+def todo():
+    path = api_path(f"/api/v0/todo")
     r = requests.get(path, headers=headers, auth=auth)
 
     r.raise_for_status()
@@ -61,8 +68,8 @@ def todo(local: bool = False):
     print(result)
 
 
-def complete(arguments: List[str], local: bool = False, fish: bool = False):
-    path = api_path(f"/api/v0/completion?q={'+'.join(arguments)}", local=local)
+def complete(arguments: List[str], fish: bool = False):
+    path = api_path(f"/api/v0/completion?q={'+'.join(arguments)}")
     r = requests.get(path, headers=headers, auth=auth)
 
     r.raise_for_status()
@@ -77,14 +84,14 @@ def complete(arguments: List[str], local: bool = False, fish: bool = False):
             print(f"{s['line']}")
 
 
-def web_path(path: str = "/", local: bool = False):
-    host = "http://localhost:5000" if local else "www.coo.ee"
-    return f"https://{host}{path}"
+def web_path(path: str = "/"):
+    host = "http://localhost:5000" if local else "https://www.coo.ee"
+    return f"{host}{path}"
 
 
-def api_path(path: str = "/", local: bool = False):
-    host = "http://localhost:8080" if local else "api.coo.ee"
-    return f"https://{host}{path}"
+def api_path(path: str = "/"):
+    host = "http://localhost:8080" if local else "https://api.coo.ee"
+    return f"{host}{path}"
 
 
 if __name__ == '__main__':
