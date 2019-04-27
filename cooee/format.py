e5159176e1c15e23d27dccfd2571d95b5e131d4b
Yuri Schimke <yuri@schimke.ee>
2019-04-27 20:26:22 +0100

    Formatting fixes

--- cooee/format.py
+++ cooee/format.py
@@ -5,29 +5,26 @@ from prompt_toolkit.formatted_text import FormattedText
 
 def todo_string(todo: Dict[str, any]):
     if todo.get("status_code") == 404:
-        return FormattedText([('red', todo["message"])])
+        return FormattedText([('orange', todo["message"])])
 
-    result = FormattedText([
-        (todo.get('color', ''), todo['line']),
-        ('', ': '),
-    ])
+    result = FormattedText([])
 
-    if "description" in todo:
-        result.extend([
-            ('', ' '),
-            ('', todo.get('description')),
-        ])
+    if "line" in todo:
+        result.append((todo.get('color', ''), todo['line']))
 
     if "description" in todo:
-        result.extend([
-            ('', '\n'),
-            ('blue', todo.get('location', '')),
-        ])
+        if result:
+            result.append(('', ': '))
+        result.append(('', todo.get('description')))
+
+    if "location" in todo:
+        if result:
+            result.append(('', '\n'))
+        result.append(('blue', todo.get('location', '')))
 
     if "message" in todo:
-        result.extend([
-            ('', '\n'),
-            ('', todo.get('message', '')),
-        ])
+        if result:
+            result.append(('', '\n'))
+        result.append(('', todo.get('message', '')))
 
     return result
